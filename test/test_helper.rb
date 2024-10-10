8349f5650b223cd0dde6a8dee4d22a05332c2086
Colby Swandale <996377+colby-swandale@users.noreply.github.com>
2024-10-10 23:58:33 +1100

    resolve bug when using eager loading

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -85,8 +85,13 @@ class ActiveSupport::TestCase
   parallelize(workers: :number_of_processors)
 
   parallelize_setup do |worker|
+    Version.reset_column_information # TODO: Remove once https://github.com/rails/rails/pull/52703 is released
+
     SemanticLogger.reopen
-    Searchkick.index_suffix = "_#{worker}"
+    Searchkick.index_suffix = worker
+
+    Rubygem.reindex
+    Rubygem.disable_callbacks
   end
 
   setup do
@@ -96,9 +101,6 @@ class ActiveSupport::TestCase
 
     Unpwn.offline = true
     OmniAuth.config.mock_auth.clear
-
-    Rubygem.reindex
-    Searchkick.disable_callbacks
 
     @launch_darkly = LaunchDarkly::Integrations::TestData.data_source
     config = LaunchDarkly::Config.new(data_source: @launch_darkly, send_events: false)
